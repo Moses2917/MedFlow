d57e5929ce9a811c0877eae070db81c121f6fbcd
Movses Movsesyan <65843759+Moses2917@users.noreply.github.com>
2024-03-14 11:10:57 -0700

    Revert "Updated Config.py"
    This reverts commit 464aead1151db5fc19bcf8ab447eed91603ac596.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SECRENT_KEY = 'dtlSJ8vJPDUEr6kRiWF5rDEjYviZCvOsZvnqtkfcXJnHgVprFlDRV3UM5yT9F6gj'
 MONGO_URI = "mongodb+srv://appUser:1111@cluster0.qhwjzyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-String = "cluster"
+
 
