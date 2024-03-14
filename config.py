414c53558dfffb7ed928242d2591c8f7073ac6fc
Movses Movsesyan <65843759+Moses2917@users.noreply.github.com>
2024-03-14 11:00:56 -0700

    Update Config.py
    I am updating this file for demonstration purposes.

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 SECRENT_KEY = 'dtlSJ8vJPDUEr6kRiWF5rDEjYviZCvOsZvnqtkfcXJnHgVprFlDRV3UM5yT9F6gj'
 MONGO_URI = "mongodb+srv://appUser:1111@cluster0.qhwjzyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-# import os
-# SECRET_KEY = os.urandom(32)
+String  = "HI"
 
