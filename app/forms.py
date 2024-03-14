610cd46ab6b909ba038571dba19ce52afbbdc159
Movses Movsesyan <65843759+Moses2917@users.noreply.github.com>
2024-03-14 11:49:12 -0700

    Updated Forms.py
    Added email-validator package

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import Email, EqualTo, DataRequired
+import email_validator
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[Email(), DataRequired()])
