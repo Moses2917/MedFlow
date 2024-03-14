87a9aaee8313b847f75d52eaf15f99d25f274066
MrChrisJr <mrchrisjr1024@gmail.com>
2024-03-14 11:35:47 -0700

    Spelling errors

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-SECRENT_KEY = 'dtlSJ8vJPDUEr6kRiWF5rDEjYviZCvOsZvnqtkfcXJnHgVprFlDRV3UM5yT9F6gj'
+SECRET_KEY = 'dtlSJ8vJPDUEr6kRiWF5rDEjYviZCvOsZvnqtkfcXJnHgVprFlDRV3UM5yT9F6gj'
 MONGO_URI = "mongodb+srv://appUser:1111@cluster0.qhwjzyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
