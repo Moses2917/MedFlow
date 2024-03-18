3cfd695f3ee333f81950295dcd17a12457ec27e4
Movses Movsesyan <65843759+Moses2917@users.noreply.github.com>
2024-03-17 21:42:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import secrets
 SECRET_KEY = secrets.token_urlsafe(32)
-MONGO_URI = "INSERTURI"
+MONGO_URI = "mongodb+srv://{}@cluster0.qhwjzyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format("INSERT_USER:PASS")
