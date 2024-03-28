d9635e05a61e60960771b01d2e5ddf8aa80243ce
Movses Movsesyan <65843759+Moses2917@users.noreply.github.com>
2024-03-28 11:32:23 -0700

    Update config.py
    Now you must store the Username and Password in your computer's path variables

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 import secrets
+from os import environ as ENV
 SECRET_KEY = secrets.token_urlsafe(32)
-MONGO_URI = "mongodb+srv://{}@cluster0.qhwjzyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format("INSERT_USER:PASS")
+MONGO_URI = "mongodb+srv://{}@cluster0.qhwjzyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format(ENV.get('mongo_user')+":"+ENV.get('mongo_pass'))
